c661d010481bfbf46f117f798e76af2f7ba63044
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-08-11 23:55:41 -0400

    navigation fixed

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='flex items-center justify-between p-5 shadow-sm border-b-[1px]'>
+        <Image src='/car_rental_logo_small.png'
+        alt = 'logo'
+        width={150}
+        height={150}
+        />
+
+        <div className="hidden md:flex gap-5">
+            <h2 className='hover:bg-blue-500 p-2 rounded-full hover:text-white px-3 hover:cursor-pointer'>Home</h2>
+            <h2 className='hover:bg-blue-500 p-2 rounded-full hover:text-white px-3 hover:cursor-pointer'>History</h2>
+            <h2 className='hover:bg-blue-500 p-2 rounded-full hover:text-white px-3 hover:cursor-pointer'>Contact Us</h2>
+        </div>
+
+        <UserButton />
+
+    </div>
+  )
+}
+
+export default NavBar
